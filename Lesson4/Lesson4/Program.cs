c0432a04ca67272ed66b0ef3a2dd9024c91bb333
Anastasija <103065344+anastasijakrjanina@users.noreply.github.com>
2022-05-04 19:46:37 +0300

    ,,

---
+++ Lesson4/Lesson4/Program.cs
@@ -0,0 +1,98 @@
+﻿
+Console.WriteLine("Start");
+Console.WriteLine();
+
+//string userAgeText = Console.ReadLine();
+//int age = int.Parse(userAgeText);
+
+//if (age >= 18)
+//{
+//    Console.WriteLine("You are a teenager.");
+//}
+//else if (age >= 13)
+//{
+//    Console.WriteLine("You are an adult.");
+//}
+//else
+//{
+//    Console.WriteLine("You are a kid.")
+//}
+
+
+Console.WriteLine("Enter your name.");
+string name = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(name))
+{
+    
+    Console.WriteLine("You did not enter your name");
+    Console.WriteLine("Try again.");
+
+    name = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace (name))
+    {
+        Console.WriteLine("No more tries");
+    }
+    else
+    {
+        Console.WriteLine($"Your name is {name}.");
+    }
+}
+else
+{
+    Console.WriteLine($"Your name is {name}.");
+}
+
+
+//int age2 = 3;
+//switch (age2)
+//{
+//    case 1:
+//        Console.WriteLine("Case 1");
+//        break;
+//    case 2:
+//        Console.WriteLine("Case 2");
+//        break;
+//    case 3:
+//        Console.WriteLine("Case 3");
+//        break;
+//    case -1:
+//        Console.WriteLine("Negative age");
+//        break ;
+//    case 0:
+//        Console.WriteLine("Baby");
+//        break;
+//    default:
+//        Console.WriteLine("Didn't get.");
+//        break;
+//}
+
+
+//string name2 = "Aa";
+//switch (name2)
+//{
+//    case "Aa": //jesli "Aa" ILI "Bb"
+//    case "Bb":
+//        Console.WriteLine("Case 1");
+//        Console.WriteLine("Case 2");
+//        break;
+//    case "Cc":
+//        Console.WriteLine("Case 3");
+//        break;
+//    case "Dd":
+//        Console.WriteLine("Negative age");
+//        break;
+//    case "EE":
+//        Console.WriteLine("Baby");
+//        break;
+//    default:
+//        Console.WriteLine("Didn't get.");
+//        break;
+//}
+
+
+Console.WriteLine();
+Console.WriteLine("End");
+
+
---
+++ Lesson5/CiklsMassives/Program.cs
@@ -0,0 +1,64 @@
+﻿int[] nums = new int[3];
+
+Console.WriteLine($"{nums[0]} {nums[1]} {nums[2]}");
+
+nums[0] = 15;
+nums[1] = 5;
+nums[2] = 8;
+
+Console.WriteLine($"{nums[0]} {nums[1]} {nums[2]}");
+
+string[] fruits = { "Apple", "Orange" };
+
+Console.WriteLine($"{fruits[0]} {fruits[1]}");
+
+
+Console.WriteLine();
+Console.WriteLine("===================================================");
+Console.WriteLine();
+
+for (int i = 0; i < nums.Length; i++)
+{
+    int num = nums[i];
+    Console.WriteLine($"i = {i} num = {num}");
+}
+
+Console.WriteLine();
+Console.WriteLine("===================================================");
+Console.WriteLine();
+
+foreach (int num in nums)
+{
+    Console.WriteLine($"number is = {num}");
+}
+
+Console.WriteLine();
+Console.WriteLine("===================================================");
+Console.WriteLine();
+
+
+int whileIterator = 0;
+while (whileIterator < nums.Length)
+{
+    int num = nums[whileIterator];
+    Console.WriteLine($"whileIterator = {whileIterator} num = {num}");
+    whileIterator++;
+}
+
+Console.WriteLine();
+Console.WriteLine("===================================================");
+Console.WriteLine();
+
+int userNumber;
+bool isNumberEntered = false;
+
+do
+{
+    Console.WriteLine("Lūdzu ievadi veselu skaitli?");
+    string userNumberText = Console.ReadLine();
+    isNumberEntered = int.TryParse(userNumberText, out userNumber);
+
+} while (!isNumberEntered);
+
+Console.WriteLine($"You entered {userNumber}");
+
