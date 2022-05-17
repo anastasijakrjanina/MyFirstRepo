f1dbec1ba712ca9016ad422a515d163fed50ca42
Anastasija <103065344+anastasijakrjanina@users.noreply.github.com>
2022-05-17 20:18:42 +0300

    ..

---
+++ ScopeLesson7/ScopesLesson/Configuration.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScopesLesson
+{
+    public class Configuration
+    {
+        public static string AppName = "Macamies par scopiem";
+
+        public static void Sum()
+        {
+            Console.WriteLine("Sum was called");
+        }
+    }
+}
+//how was before .NET 6
+//namespace ScopesLession
+//{
+//    public static class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            int number = 15;
+
+//            if (number > 45)
+//            {
+//                int number2 = number + 5;
+//                Console.WriteLine($"Mans skaitlis ir: {number2}");
+
+//                if (number2 < 45)
+//                {
+//                    int number3 = number2 + 8;
+//                    Console.WriteLine($"Mans skaitlis ir: {number3}");
+//                }
+
+//                {
+//                    int number4 = number2 + 9;
+//                    Console.WriteLine($"Mans skaitlis ir: {number4}");
+//                }
+//            }
+//            Console.WriteLine($"Mans skaitlis ir: {number}");
+
+//            string appName = Configuration.AppName;
+//            Console.WriteLine($"Aplikācijas nosaukums ir {appName}");
+
+//            // Piemērs ja nebūtu static
+//            // Configuration configuration = new Configuration();
+//            //configuration.Sum();
+
+//            Configuration.Sum();
+//        }
+//    }
+//}
---
+++ ScopeLesson7/ScopesLesson/Program.cs
@@ -0,0 +1,31 @@
+﻿using ScopesLesson;
+
+int number = 15;
+
+if (number > 45)
+{
+    int number2 = number + 5;
+    Console.WriteLine($"Mans skaitlis ir: {number2}");
+
+    if (number2 < 45)
+    {
+        int number3 = number2 + 8;
+        Console.WriteLine($"Mans skaitlis ir: {number3}");
+    }
+
+    {
+        int number4 = number2 + 9;
+        Console.WriteLine($"Mans skaitlis ir: {number4}");
+    }
+}
+
+Console.WriteLine($"Mans skaitlis ir: {number}");
+
+string appName = Configuration.AppName;
+Console.WriteLine($"Aplikacijas nosaukums ir {appName}");
+
+// Piemērs ja nebūtu static
+//Configuration configuration = new Configuration();
+//configuration.Sum();
+
+Configuration.Sum();
